f8895c6618f50b306f276bb78ba360192a1b2cab
Michael Dobak <mdobak44@icloud.com>
2021-02-05 20:35:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |badges| 
+  "Hello, my name is #{badges}."
+  end 
+end
+
+def assign_rooms(speakers)
+  count = 1 
+  speakers.map do |speaker|
+    return "Hello, #{speaker}! You'll be assigned to room #{count}!"
+  count += 1 
+end
+end
