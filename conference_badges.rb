19f1d4253eb523669a6e3097038e946dd4064587
Michael Dobak <mdobak44@icloud.com>
2021-02-05 20:46:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 def assign_rooms(speakers)
   new_array = [ ]
   speakers.each_with_index do |badge, i| 
-    new_array << "Hello, #{name}! You'll be assigned  to room #{i + 1}!"
+    new_array << "Hello, #{badge}! You'll be assigned  to room #{i + 1}!"
   end
   new_array
 end
